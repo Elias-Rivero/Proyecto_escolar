20f32bf244fa24eef8fc215dcdaf45903c7f3429
Isa_Borges <157186642+joseantoniocr7@users.noreply.github.com>
2024-09-09 11:24:15 -1100

    Estableciendo PnlEmpleado(jose)

--- src/es/ies/pnlTienda.java
+++ src/es/ies/pnlTienda.java
@@ -51,33 +51,37 @@ public class pnlTienda extends javax.swing.JPanel {
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(201, 201, 201)
-                .addComponent(bntComprar, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(248, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(btnTicket, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(62, 62, 62))
-        );
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGap(201, 201, 201)
+                                .addComponent(bntComprar, javax.swing.GroupLayout.PREFERRED_SIZE, 153,
+                                        javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap(248, Short.MAX_VALUE))
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(btnTicket, javax.swing.GroupLayout.PREFERRED_SIZE, 127,
+                                        javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(62, 62, 62)));
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(397, Short.MAX_VALUE)
-                .addComponent(bntComprar, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(45, 45, 45)
-                .addComponent(btnTicket, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(45, 45, 45))
-        );
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addContainerGap(397, Short.MAX_VALUE)
+                                .addComponent(bntComprar, javax.swing.GroupLayout.PREFERRED_SIZE, 55,
+                                        javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(45, 45, 45)
+                                .addComponent(btnTicket, javax.swing.GroupLayout.PREFERRED_SIZE, 40,
+                                        javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(45, 45, 45)));
     }
-/**
- * 
- * @param evt 
- * Conecta con la bbdd e introduce en el fichero pdf párrafos con los datos de cada producto.
- */
+
+    /**
+     * 
+     * @param evt
+     *            Conecta con la bbdd e introduce en el fichero pdf párrafos con los
+     *            datos de cada producto.
+     */
     private void bntComprarActionPerformed(java.awt.event.ActionEvent evt) {
-        try  {
+        try {
             this.id = JOptionPane.showInputDialog("Introduce el ID del producto a comprar");
             this.cantidad = JOptionPane.showInputDialog("Introduce la cantidad");
             cantidadNum = cantidadNum + Integer.parseInt(this.cantidad);
@@ -85,15 +89,14 @@ public class pnlTienda extends javax.swing.JPanel {
             cli.participarSorteo(cli.getIdCliente(), cantidadNum);
             String sqlNombre = "select * from productos where id = '" + this.id + "'";
 
-
             String id = "id";
             String nom = "nombre";
             String fec = "fecha_cad";
 
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "Error de operación " + e.getMessage(), "Sentencia SQL", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Error de operación " + e.getMessage(), "Sentencia SQL",
+                    JOptionPane.ERROR_MESSAGE);
         }
-
 
     }
 
@@ -117,13 +120,14 @@ public class pnlTienda extends javax.swing.JPanel {
                     Desktop.getDesktop().open(path);
                     dia.setVisible(false);
                 } catch (IOException ex) {
-                    JOptionPane.showMessageDialog(null, "Error de operación " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Error de operación " + ex.getMessage(), "Error",
+                            JOptionPane.ERROR_MESSAGE);
                 }
             }
         });
 
-
     }
+
     private javax.swing.JButton bntComprar;
     private javax.swing.JButton btnTicket;
 
@@ -133,13 +137,11 @@ public class pnlTienda extends javax.swing.JPanel {
         try {
             String sqlProductos = "select ID ,NOMBRE ,TO_CHAR(fecha_cad)\"Fecha de Caducidad\" from productos order by nombre";
 
-
-
             int numeroColumnasP = 5;
             Object[] etiquetasP = new Object[numeroColumnasP];
             modeloProductos.setColumnIdentifiers(etiquetasP);
             int n = 1;
-            while (n<3) {
+            while (n < 3) {
                 Object[] filaP = new Object[numeroColumnasP];
                 for (int i = 0; i < numeroColumnasP; i++) {
                     System.out.println(n);
@@ -147,19 +149,22 @@ public class pnlTienda extends javax.swing.JPanel {
                 modeloProductos.addRow(filaP);
                 n++;
             }
-            // Se añade al modelo la fila completa.
+
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "Error de operación " + e.getMessage(), "Sentencia SQL", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Error de operación " + e.getMessage(), "Sentencia SQL",
+                    JOptionPane.ERROR_MESSAGE);
         }
         JScrollPane tbCajaP = new JScrollPane(tablaProductos);
         tbCajaP.setBounds(2, 0, 582, 300);
         this.add(tbCajaP);
         tablaProductos.setEnabled(false);
     }
-/**
- * Genera un cliente a partir del id que se logueó (previamente introducido en un fichero
- * lo lee y busca sus datos en la bbdd.
- */
+
+    /**
+     * Genera un cliente a partir del id que se logueó (previamente introducido en
+     * un fichero
+     * lo lee y busca sus datos en la bbdd.
+     */
     private void generarCliente() {
         File f2 = new File("nombreCli.txt");
 
